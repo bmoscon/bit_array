74ba36a528738fb60563d37fd2189acba277e21c
Bryant Moscon <bmoscon@gmail.com>
2016-09-17 19:21:41 -0400

    work in progress

--- bit_array/bit_array.py
+++ bit_array/bit_array.py
@@ -16,13 +16,28 @@ class BitArray(object):
         # so lets look up the size of L on this machine
         self.elem_size = int(log(array.array('L').itemsize * 8, 2))
         self.elem_mask =  int(pow(2, self.elem_size) - 1)
+        if isinstance(bits, list):
+            data = list(bits)
+            bits = len(data)
+        else:
+            data = None   
         size = bits >> self.elem_size;
         if bits & self.elem_mask:
             size += 1
-        
+        print size
+        self.size = bits
         self.bits = array.array('L', (0,) * size)
+        if data is not None:
+            for i in xrange(0, len(data), self.elem_size):
+                print i, self.elem_size
+                print data[i:i+self.elem_size]     
+       
+    def __len__(self):
+        return self.size
 
     def __setitem__(self, index, value):
+        if index > self.size:
+            raise IndexError
         array_index = index >> self.elem_size
         bit_position = index & self.elem_mask
         mask = 1 <<  bit_position
@@ -33,8 +48,13 @@ class BitArray(object):
             self.bits[array_index] &= mask
 
     def __getitem__(self, index):
-        array_index = index >> self.elem_size
-        bit_position = index & self.elem_mask
-        mask = 1 << bit_position
-        return 1 if self.bits[array_index] & mask else 0
+        if isinstance(index, slice):
+            return [self[x] for x in xrange(*index.indices(len(self)))]
+        else:
+            if index >= self.size:
+                raise IndexError
+            array_index = index >> self.elem_size
+            bit_position = index & self.elem_mask
+            mask = 1 << bit_position
+            return 1 if self.bits[array_index] & mask else 0
 
