8caabf54fdce5ef35e5b14e5167c1370932b8bb8
Bryant Moscon <bmoscon@gmail.com>
2016-09-17 20:06:13 -0400

    Slicing fixed. Init also takes a size or a list

--- bit_array/bit_array.py
+++ bit_array/bit_array.py
@@ -8,30 +8,29 @@ associated with this software.
 '''
 
 import array
-from math import log
+from math import ceil
 
 class BitArray(object):
     def __init__(self, bits):
         # L = 4 bytes minimum, but could be 8 bytes. No need to waste all that space
         # so lets look up the size of L on this machine
-        self.elem_size = int(log(array.array('L').itemsize * 8, 2))
-        self.elem_mask =  int(pow(2, self.elem_size) - 1)
+        self.elem_size = array.array('L').itemsize * 8
+        self.elem_mask =  self.elem_size - 1
+        
         if isinstance(bits, list):
             data = list(bits)
             bits = len(data)
         else:
             data = None   
-        size = bits >> self.elem_size;
-        if bits & self.elem_mask:
-            size += 1
-        print size
+        size = int(ceil(float(bits) / float(self.elem_size)))
         self.size = bits
         self.bits = array.array('L', (0,) * size)
         if data is not None:
+            index = 0
             for i in xrange(0, len(data), self.elem_size):
-                print i, self.elem_size
-                print data[i:i+self.elem_size]     
-       
+                self.bits[index] = int("".join(str(x) for x in data[i:i+self.elem_size])[::-1],2)
+                index += 1
+
     def __len__(self):
         return self.size
 
@@ -49,7 +48,7 @@ class BitArray(object):
 
     def __getitem__(self, index):
         if isinstance(index, slice):
-            return [self[x] for x in xrange(*index.indices(len(self)))]
+            return BitArray([self[x] for x in xrange(*index.indices(len(self)))])
         else:
             if index >= self.size:
                 raise IndexError
